4ea23dba6e26b413464a2f8061b4295e690459be
tsioumioupiou <g-anastasiou@hotmail.com>
2026-05-09 16:31:23 +0200

    Add top margin to push burger and logo down

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
     <>
       <nav className="fixed top-0 w-full z-50 bg-transparent">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center h-16 gap-4">
+          <div className="flex items-center h-16 gap-4 mt-4">
 
             {/* Burger button */}
             <div className="relative z-[60]">
